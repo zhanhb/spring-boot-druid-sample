9f6404db1a6fbec908f93280d70a135446f05878
zhanhb <zhanhb8@qq.com>
2015-08-26 22:17:58 +0800

    Initial commit

---
+++ src/main/java/com/github/zhanhb/customdatasource/Application.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2015 zhanhb.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.zhanhb.customdatasource;
+
+import java.io.IOException;
+import java.util.Locale;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.EmbeddedWebApplicationContext;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * Boot class for both web application and spring-boot application.
+ *
+ * @author zhanhb
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) throws IOException {
+        ApplicationContext context = SpringApplication.run(Application.class, args);
+        if (log.isInfoEnabled() && context instanceof EmbeddedWebApplicationContext) {
+            int port = ((EmbeddedWebApplicationContext) context).getEmbeddedServletContainer().getPort();
+            String contextPath = context.getApplicationName();
+            String url = String.format(Locale.US, "http://localhost:%d%s", port, contextPath);
+            String dashes = "------------------------------------------------------------------------";
+            log.info("Access URLs:\n{}\n\tLocal: \t\t{}\n{}", dashes, url, dashes);
+        }
+
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.showBanner(false)
+                .addCommandLineProperties(false)
+                .logStartupInfo(true)
+                .sources(Application.class);
+    }
+
+}
